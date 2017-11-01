8d0d7be3248393c273f2a1cce5c9126ac83f2088
wasalaza <wilfersalazar19@gmail.com>
2017-10-31 20:36:04 -0500

    Ajustes finales

--- src/components/iframeVideo/iframeVideo.js
+++ src/components/iframeVideo/iframeVideo.js
@@ -15,7 +15,7 @@ class FrameVideo extends Component {
         const { linkVideo, height, paddingTop } = this.props;
         return (
             <Col xs={12} md={12} lg={12} style={{ paddingTop: paddingTop }}>
-                <iframe style={{ "width": "100%", "height": height, "overflow": "scroll" }} src={linkVideo}></iframe>
+                <iframe style={{ "width": "100%", "height": height, "overflow": "scroll" }} src={linkVideo + '?rel=0&amp;showinfo=0'}></iframe>
             </Col>
         );
     }
